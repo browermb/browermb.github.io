f4655b2fcd693edab51c09d476bf2571813766dc
maddie brower <maddiebrower@Dobby.local>
2019-02-09 07:58:32 -0500

    reorganized and added information to week 5 of C4k

---
+++ collegeforkids-python/madlib.py
@@ -0,0 +1,10 @@
+print("A vacation is when you take a trip to some (adjective) play with your (adjective) family.")
+print("Usually you go to some place that is near a/an (noun) or up on a/an (noun).")
+print("A good vacation place is one  where you can ride (plural noun) or play (game) or go hunting for (plural noun).")
+print("I like to spend my time (verb-ing) or (verb-ing).")
+print("When parents go on a vacation, they spend their time eating three (plural noun) a day, " +
+      "and fathers play golf, and mothers sit around (verb -ing)")
+print("Last summer, my little brother fell in a/an (noun) and got poision (plant) all over his (body part).")
+print("My family is going to go to (a place), and I will practice (verb-ing).")
+print("Parents need vacations more than kids because parents are always very (adjective) " +
+      "and because they have to work (number) hours every day all year making enough (plural noun) to par for the vacation.")
