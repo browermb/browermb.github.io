b7f59dde01953ea7bffee26c1ff48a7e6b4c93ed
maddie brower <maddiebrower@Dobby.local>
2019-02-28 22:28:02 -0500

    made small changes added some more information, removed on scroll part and fixed dropdown of navbar, when navbar moves it breaks

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function navbarLocater() {
     nav = document.getElementById("homeNavbar");
     image = document.getElementById("reddishKnob");
     if (window.scrollY > "300") {
-        nav.style.top = "0px";
+        nav.style.top = window.screenTop;
         //need to fix the nav bar to top of the page
     } else if (window.scrollY < "300") {
         nav.style.top = "300px";
